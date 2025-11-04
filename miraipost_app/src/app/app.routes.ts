836711e7abb6926d485fcc553a8ef122226ebaae
YinMyatThwe <yinmyatthwe1224@gmail.com>
2025-11-04 11:35:59 +0900

    ホーム、ロックイン、登録components 作成

--- miraipost_app/src/app/app.routes.ts
+++ miraipost_app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];

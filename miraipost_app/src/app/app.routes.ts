773d35e808599d841c5fa5d1e3aa0735e1bc5ffe
takeda michika <mi.chi.ka10@icloud.com>
2025-11-11 11:42:07 +0900

    profile#8

--- miraipost_app/src/app/app.routes.ts
+++ miraipost_app/src/app/app.routes.ts
@@ -4,9 +4,12 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+
 ];

efb74e057b35f694c010549e67c51be931e0fea4
YinMyatThwe <yinmyatthwe1224@gmail.com>
2025-11-11 12:00:06 +0900

    route

--- miraipost_app/src/app/components/header/header.component.ts
+++ miraipost_app/src/app/components/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-
 @Component({
   selector: 'app-header',
   standalone: true,
